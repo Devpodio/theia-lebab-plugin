1b7733ad67fb3db04062fcf86f07e640292dda4e
Uni Sayo <unibtc@gmail.com>
2018-12-31 21:00:57 +0000

    bump 0.0.5

--- src/lebab-plugin-backend.ts
+++ src/lebab-plugin-backend.ts
@@ -1,100 +1,46 @@
 import * as theia from '@devpodio/plugin';
-import * as fs from 'fs-extra';
-const lebab = require('lebab');
+import lebab = require('lebab');
 
 export function start(context: theia.PluginContext) {
     const lebabFormatCommand = {
         id: 'lebab-format',
         label: 'Format Document via Lebab'
     };
-    const lebabConfigCommand = {
-        id: 'lebab-config',
-        label: 'Creat or Open a .lebabrc.json configuration file on your workspace root directory'
-    }
-    const documentSelector = { language: 'javascript' };
-    const documentFormattingProvider = {
-        provideDocumentFormattingEdits: async (document: theia.TextDocument, options: theia.FormattingOptions) => {
-            console.log('---> formatting the document');
-            const result: theia.TextEdit[] = [];
-            const content = document.getText();
-            const workspaces = theia.workspace.workspaceFolders;
-            try {
-                let ws;
-                if (workspaces && workspaces[0]) {
-                    ws = workspaces[0].uri.path.toString();
-                } else {
-                    console.log('Workspace not found, Lebab formatting disabled');
-                    return result;
-                }
-                const configPath = ws + '/.lebabrc.json';
-                const uri = await fs.pathExists(configPath);
-                let configRc;
-                if (!uri) {
-                    configRc = theia.workspace.getConfiguration('lebab');
-                } else {
-                    configRc = await fs.readJson(configPath) || {};
-                }
-                if (!configRc.enabled) {
-                    return result;
-                }
-                let transformed;
-                try {
-                    if (!configRc.transforms) {
-                        return result;
-                    }
-                    transformed = lebab.transform(content, configRc.transforms);
-                } catch (e) {
-                    if (configRc.showErrors) {
-                        theia.window.showErrorMessage(e.message);
-                    }
-                    return result;
-                }
-                const { code, warning } = transformed;
-                if (warning && configRc.showWarnings) {
-                    theia.window.showWarningMessage(JSON.stringify(warning, null, 2));
-                }
-                if (!code) {
-                    return result;
-                }
-                const positionStart = new theia.Position(0, 0);
-                const positionEnd = new theia.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-                const range = new theia.Range(positionStart, positionEnd);
-                result.push(new theia.TextEdit(range, code));
-                return result;
-
-            } catch (e) {
-                console.log('Lebab extension error occured', e.message);
-                return result;
-            }
-        }
-    };
-
+    const documentSelector: theia.DocumentSelector = { scheme: 'file', language: 'typescript' };
+    const documentFormattingProvider = { provideDocumentFormattingEdits: provideFormatting };
     context.subscriptions.push(
         theia.languages.registerDocumentFormattingEditProvider(documentSelector, documentFormattingProvider)
     );
-    theia.commands.registerCommand(lebabFormatCommand, (...args: any[]) => {
-        theia.commands.executeCommand('editor.action.formatDocument');
-    });
-    theia.commands.registerCommand(lebabConfigCommand, (...args: any[]) => {
-        const workspaces = theia.workspace.workspaceFolders;
-        let ws;
-        if (workspaces && workspaces[0]) {
-            ws = workspaces[0].uri.path.toString();
-        } else {
-            console.log('Workspace not found, Lebab formatting disabled');
-            theia.window.showErrorMessage('Workspace not found, Lebab formatting disabled');
+    context.subscriptions.push(
+        theia.commands.registerCommand(lebabFormatCommand, () => {
+            theia.commands.executeCommand('editor.action.formatDocument');
+        })
+    )
+}
+async function provideFormatting(document: theia.TextDocument) {
+    const preferences = theia.workspace.getConfiguration('lebab');
+    const result: theia.TextEdit[] = [];
+    const content = document.getText();
+    const workspaceFolders = theia.workspace.workspaceFolders;
+    if (!preferences.get('enabled')) {
+        return result;
+    }
+    const transforms = preferences.get('transforms');
+    if (workspaceFolders && workspaceFolders[0]) {
+        const { code, warning } = lebab.transform(content, transforms);
+        if (warning && preferences.get('showWarnings')) {
+            theia.window.showWarningMessage(JSON.stringify(warning, null, 2));
         }
-        const configPath = ws + '/.lebabrc.json';
-        const uri = theia.Uri.parse('untitled:' + configPath);
-        theia.workspace.openTextDocument(uri).then(doc => {
-            if (doc) {
-                theia.window.showTextDocument(doc);
-            }
-        });
-    });
-};
-
+        if (!code) {
+            return result;
+        }
+        const positionStart = new theia.Position(0, 0);
+        const positionEnd = new theia.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+        const range = new theia.Range(positionStart, positionEnd);
+        result.push(new theia.TextEdit(range, code));
+        return result;
+    }
+}
 export function stop() {
 
 }
-
