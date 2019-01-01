8c920cd753064f7c114b9b7410719a613ee19856
Uni Sayo <unibtc@gmail.com>
2019-01-01 11:01:31 +0000

    release 0.0.7

--- src/lebab-plugin-backend.ts
+++ src/lebab-plugin-backend.ts
@@ -6,7 +6,7 @@ export function start(context: theia.PluginContext) {
         id: 'lebab-format',
         label: 'Format Document via Lebab'
     };
-    const documentSelector: theia.DocumentSelector = { scheme: 'file', language: 'typescript' };
+    const documentSelector = { language: 'javascript' };
     const documentFormattingProvider = { provideDocumentFormattingEdits: provideFormatting };
     context.subscriptions.push(
         theia.languages.registerDocumentFormattingEditProvider(documentSelector, documentFormattingProvider)
@@ -17,28 +17,30 @@ export function start(context: theia.PluginContext) {
         })
     )
 }
-async function provideFormatting(document: theia.TextDocument) {
+function provideFormatting(document: theia.TextDocument) {
     const preferences = theia.workspace.getConfiguration('lebab');
     const result: theia.TextEdit[] = [];
     const content = document.getText();
     const workspaceFolders = theia.workspace.workspaceFolders;
-    if (!preferences.get('enabled')) {
-        return result;
-    }
-    const transforms = preferences.get('transforms');
-    if (workspaceFolders && workspaceFolders[0]) {
-        const { code, warning } = lebab.transform(content, transforms);
-        if (warning && preferences.get('showWarnings')) {
-            theia.window.showWarningMessage(JSON.stringify(warning, null, 2));
-        }
-        if (!code) {
-            return result;
-        }
-        const positionStart = new theia.Position(0, 0);
-        const positionEnd = new theia.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-        const range = new theia.Range(positionStart, positionEnd);
-        result.push(new theia.TextEdit(range, code));
-        return result;
+    try {
+      if (!preferences.get('enabled')) {
+          return result;
+      }
+      const transforms = preferences.get('transforms');
+      if (workspaceFolders && workspaceFolders[0]) {
+          const { code, warning } = lebab.transform(content, transforms);
+          if (warning && preferences.get('showWarnings')) {
+              theia.window.showWarningMessage(JSON.stringify(warning, null, 2));
+          }
+          if (!code) {
+              return result;
+          }
+          const positionStart = new theia.Position(0, 0);
+          const positionEnd = new theia.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+          const range = new theia.Range(positionStart, positionEnd);
+          result.push(new theia.TextEdit(range, code));
+          return result;
+      }
     }
 }
 export function stop() {
