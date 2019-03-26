9389e5207f1dbf37afdd8e615fe493c72741d23b
Uni Sayo <unibtc@gmail.com>
2019-03-26 14:39:18 +0000

    add additional beautify options
    Signed-off-by: Uni Sayo <unibtc@gmail.com>

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,6 +7,20 @@ interface BeautifyConfig {
   brace_style: string;
   unindent_chained_methods: boolean;
   keep_array_indentation: boolean;
+  indent_char: string;
+  indent_with_tabs: boolean;
+  end_with_newline: boolean;
+  indent_level: number;
+  space_in_paren: boolean;
+  space_in_empty_paren: boolean;
+  jslint_happy: boolean;
+  space_after_named_function: boolean;
+  break_chained_methods: boolean;
+  unescape_strings: boolean;
+  wrap_line_length: number;
+  comma_first: boolean;
+  operator_position: string;
+  indent_empty_lines: boolean;
 }
 
 export interface LebabConfig {
